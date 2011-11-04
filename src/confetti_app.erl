e6a98f8d6efb49859afeb2e081796e11091e4e84
Adam Rutkowski <hq@mtod.org>
2011-11-04 14:15:12 +0100

    add confetti_app:start/0

--- src/confetti_app.erl
+++ src/confetti_app.erl
@@ -3,11 +3,16 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/0, start/2, stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
+
+% useful for console starts
+% i.e. erl -pa apps/*/ebin -pa deps/*/ebin -boot start_sasl -s confetti_app
+start() ->
+    application:start(confetti).
 
 start(_StartType, _StartArgs) ->
     confetti_sup_sup:start_link().
